ca7b3c79a3633db32ae64aae583d959ad25f0d72
alexis <alexisrodriguezarteaga@gmail.com>
2022-08-10 12:50:57 -0300

    change form tag to div and remove padding

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ const ipc = require('./ipcModule');
 const app = express();
 
 //Settings
-app.set('appName', 'Dollar calculator')
+app.set('appName', 'Inflation calculator');
 app.set('port', '3000');
 
 // Middlewares
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 //Rutas
 app.post('/api', (req, res) => {
     const datos = req.body;
-    
-    if (ipc.validateData(datos)){
-        const datosRes = {precio: ipc.calculatePrice(datos)};
+
+    if (ipc.validateData(datos)) {
+        const datosRes = { precio: ipc.calculatePrice(datos) };
         res.status(200).json(datosRes);
-    }
-    else
+    } else
         res.status(400).send({
-            message: 'This is an error!'
+            message: 'This is an error!',
         });
 });
- 
+
 app.listen(app.get('port'), () => {
     console.log(app.get('appName'));
     console.log('Server on port', app.get('port'));
--- public/main.js
+++ public/main.js
@@ -1,23 +1,24 @@
 //Actualiza la cantidad de meses en los selectores
-function updateMonths(){
+function updateMonths() {
     // select salva el selector de meses que vamos a actualizar, hay dos de ellos
     let select;
     // selectedMonth salva el mes seleccionado antes de actualizar el selector
     let selectedMonth;
     let num;
     //Salva el selector correspondiente segun this (quién llamo al evento)
-    if (this.id == 'year1'){
+    if (this.id == 'year1') {
         select = document.getElementById('month1');
         selectedMonth = select.value;
     }
-    if (this.id == 'year2'){
+    if (this.id == 'year2') {
         select = document.getElementById('month2');
         selectedMonth = select.value;
     }
-    
-    if (this.value == 2022) num = 3; else num = 12;
+
+    if (this.value == 2022) num = 3;
+    else num = 12;
     //Si num es distinto a la cant actual del selector a modificar, actualizar la cant de meses y salvar el valor anterior, sino no es necesario!
-    if(num != select.childElementCount){
+    if (num != select.childElementCount) {
         //Elimina todos los elems del selector de meses
         while (select.firstChild != null) {
             select.firstChild.remove();
@@ -30,7 +31,7 @@ function updateMonths(){
             select.appendChild(option);
         }
         //Si al value seleccionado anteriormente sigue siendo valido en el rango actual que seria num, seleccionar ese value, sino pone 1 por def
-        if(selectedMonth <= num){
+        if (selectedMonth <= num) {
             select.value = selectedMonth;
         }
     }
@@ -38,7 +39,7 @@ function updateMonths(){
 
 //Opciones para select de mes incial (1-3 porque el año por defecto es 2022)
 function addMonths() {
-    let selects = document.querySelectorAll('.month')
+    let selects = document.querySelectorAll('.month');
     selects.forEach(select => {
         for (let i = 1; i <= 3; i++) {
             let option = document.createElement('option');
@@ -49,8 +50,8 @@ function addMonths() {
     });
 }
 //Opciones para select de año (1913-2022)
-function addYears(){
-    let selects = document.querySelectorAll('.year')
+function addYears() {
+    let selects = document.querySelectorAll('.year');
     selects.forEach(select => {
         for (let i = 2022; i >= 1913; i--) {
             let option = document.createElement('option');
@@ -61,45 +62,45 @@ function addYears(){
     });
 }
 
-function showPrice(data){
+function showPrice(data) {
     document.getElementById('price2').value = data.precio;
 }
 
 function postFetch() {
     const data = {
-        month1: document.getElementById("month1").value,
-        month2: document.getElementById("month2").value,
-        year1: document.getElementById("year1").value,
-        year2: document.getElementById("year2").value,
-        price1: document.getElementById("price1").value
-    }
+        month1: document.getElementById('month1').value,
+        month2: document.getElementById('month2').value,
+        year1: document.getElementById('year1').value,
+        year2: document.getElementById('year2').value,
+        price1: document.getElementById('price1').value,
+    };
 
     const options = {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
-    }
-    fetch('/api',options)
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        } else{
-            document.getElementById("price2").value= "";
-            alert("Los datos ingresados son invalidos");
-            throw new Error ("Invalid data");
-        }
-    })
-    .then(json => showPrice(json))
-    .catch(err => console.log(err));
+        body: JSON.stringify(data),
+    };
+    fetch('/api', options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                document.getElementById('price2').value = '';
+                alert('Los datos ingresados son invalidos');
+                throw new Error('Invalid data');
+            }
+        })
+        .then(json => showPrice(json))
+        .catch(err => console.log(err));
 }
 
-window.onload = function() {
+window.onload = function () {
     addYears();
     addMonths();
     document.querySelectorAll('.year').forEach(item => {
         item.addEventListener('change', updateMonths);
-    })
-    document.getElementById('enviar').addEventListener('click', postFetch);    
-}
+    });
+    document.getElementById('enviar').addEventListener('click', postFetch);
+};
