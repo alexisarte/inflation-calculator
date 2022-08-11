40e10e927ecfc98da0a81d3a86695842a1770e24
alexis <alexisrodriguezarteaga@gmail.com>
2022-08-11 13:31:19 -0300

    replace the numbers of months by the respective names

--- ipcModule.js
+++ ipcModule.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const rawdata = fs.readFileSync('./bls.json');
 const bls = JSON.parse(rawdata);
-const YEAR_LAST = bls[bls.length - 1].year;
-const YEAR_FIRST = bls[0].year;
+const LAST_YEAR = bls[bls.length - 1].year;
+const FIRST_YEAR = bls[0].year;
 
 function findIpc(month, year) {
     return bls.find(e => ((e.month == month) && (e.year == year))).value;
@@ -15,16 +15,16 @@ function calculatePrice(datos) {
 }
 
 function validateData(data) {
-    let monthsOk = false;
+    let monthsOk, yearsOk;
     const regex = new RegExp(/^\d{0,10}(\.\d{0,2})?$/);
     if (regex.test(data.price1)) {
-        let yearsOk = (((data.year1 && data.year2) >= YEAR_FIRST) && (data.year1 && data.year2 <= YEAR_LAST));
+        yearsOk = (((data.year1 && data.year2) >= FIRST_YEAR) && (data.year1 && data.year2 <= LAST_YEAR));
         if (yearsOk) {
-            if (data.year1 != YEAR_LAST && data.year2 != YEAR_LAST) {
+            if (data.year1 != LAST_YEAR && data.year2 != LAST_YEAR) {
                 monthsOk = data.month1 <= 12 && data.month2 <= 12;
-            } else if (data.year1 == YEAR_LAST && data.year2 == YEAR_LAST) {
+            } else if (data.year1 == LAST_YEAR && data.year2 == LAST_YEAR) {
                 monthsOk = data.month1 <= 3 && data.month2 <= 3;
-            } else if (data.year1 == YEAR_LAST) {
+            } else if (data.year1 == LAST_YEAR) {
                 monthsOk = data.month1 <= 3 && data.month2 <= 12;
             } else {
                 monthsOk = data.month1 <= 12 && data.month2 <= 3;
--- public/main.js
+++ public/main.js
@@ -1,22 +1,28 @@
+const MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+function changeMonths(init, end, select) {
+    for (let i = init; i <= end; i++) {
+        let option = document.createElement('option');
+        option.textContent = MONTHS[i - 1];
+        option.value = i;
+        select.appendChild(option);
+    }
+}
+
 //Actualiza la cantidad de meses en los selectores
 function updateMonths() {
     // select salva el selector de meses que vamos a actualizar, hay dos de ellos
     let select, selectedMonth, numberOfMonths;
     select = this.id == 'year1' ? document.getElementById('month1') : document.getElementById('month2');
     selectedMonth = select.value;
-    numberOfMonths = this.value === 2022 ? 3 : 12;
+    numberOfMonths = this.value == 2022 ? 3 : 12;
     //Si numberOfMonths es distinto a la cant actual del selector a modificar, actualizar la cant de meses y salvar el valor anterior, sino no es necesario!
     if (numberOfMonths != select.childElementCount) {
         while (select.firstChild != null) {
             select.firstChild.remove();
         }
         //Crea 3 o 12 meses posibles en el selector segun corrresponda
-        for (let i = 1; i <= numberOfMonths; i++) {
-            let option = document.createElement('option');
-            option.textContent = i;
-            option.value = i;
-            select.appendChild(option);
-        }
+        changeMonths(1, numberOfMonths, select);
         //Si al value seleccionado anteriormente sigue siendo valido en el rango actual que seria numberOfMonths, seleccionar ese value, sino pone 1 por def
         if (selectedMonth <= numberOfMonths) {
             select.value = selectedMonth;
@@ -28,19 +34,14 @@ function updateMonths() {
 function addMonths() {
     let selects = document.querySelectorAll('.month');
     selects.forEach(select => {
-        for (let i = 1; i <= 3; i++) {
-            let option = document.createElement('option');
-            option.textContent = i;
-            option.value = i;
-            select.appendChild(option);
-        }
+        changeMonths(1, 12, select);
     });
 }
 //Opciones para select de año (1913-2022)
 function addYears() {
     let selects = document.querySelectorAll('.year');
     selects.forEach(select => {
-        for (let i = 2022; i >= 1913; i--) {
+        for (let i = 1913; i <= 2022; i++) {
             let option = document.createElement('option');
             option.textContent = i;
             option.value = i;
@@ -89,5 +90,5 @@ window.onload = function () {
     document.querySelectorAll('.year').forEach(item => {
         item.addEventListener('change', updateMonths);
     });
-    document.getElementById('enviar').addEventListener('click', postFetch);
+    document.getElementById('send').addEventListener('click', postFetch);
 };
